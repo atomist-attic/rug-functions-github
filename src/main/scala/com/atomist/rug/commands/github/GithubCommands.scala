2fb6d3135d950dca767a8ce5bd7172d644e22dae
Christian Dupuis <cd@atomist.com>
2017-01-09 02:15:34 +0100

    Some refactoring

--- src/main/scala/com/atomist/rug/commands/github/GithubCommands.scala
+++ src/main/scala/com/atomist/rug/commands/github/GithubCommands.scala
@@ -21,12 +21,21 @@ class GitHubCommands extends Command[ServicesMutableView] {
 class GitHubOperation() {
 
   def assignIssue(number: Integer, assignee: String, owner: String, repo: String, token: String): GitHubStatus = {
+    val issue = new EditIssue(number)
+    issue.setAssignee(assignee)
+    editIssue(issue, owner, repo, token)
+  }
 
+  def labelIssue(number: Integer, label: String, owner: String, repo: String, token: String): GitHubStatus = {
+    val issue = new EditIssue(number)
+    issue.addLabel(label)
+    editIssue(issue, owner, repo, token)
+  }
+
+  private def editIssue(issue: EditIssue, owner: String, repo: String, token: String): GitHubStatus = {
     val githubservices: GitHubServices = new GitHubServicesImpl(token)
 
     val repoId = new SimpleCloudRepoId(owner, repo)
-    val issue = new EditIssue(number)
-    issue.setAssignee(assignee)
 
     try {
       githubservices.editIssue(repoId, issue)
@@ -35,7 +44,8 @@ class GitHubOperation() {
     catch {
       case e: Exception => GitHubStatus(false)
     }
-  } 
+
+  }
 
   def mergePullRequest(number: Integer, owner: String, repo: String, token: String): GitHubStatus = {
     val githubservices: GitHubServices = new GitHubServicesImpl(token)
