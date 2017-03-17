0d7f04200f9422536a3e2624aa59e9ce0da198d3
Alan Stewart <alan@atomist.com>
2017-03-17 21:07:37 +1100

    Improve existing tests

--- src/test/scala/com/atomist/rug/function/github/ListUserIssuesFunctionTest.scala
+++ src/test/scala/com/atomist/rug/function/github/ListUserIssuesFunctionTest.scala
@@ -1,26 +1,30 @@
 package com.atomist.rug.function.github
 
 import com.atomist.rug.function.github.GitHubSearchIssues.GitHubIssue
+import com.atomist.rug.function.github.TestCredentials.Token
 import com.atomist.rug.spi.Handlers.Status
 import com.atomist.util.JsonUtils
-import org.scalatest.{FlatSpec, Matchers}
 
-class ListUserIssuesFunctionTest extends FlatSpec with Matchers {
+class ListUserIssuesFunctionTest extends GitHubFunctionTest(Token) {
 
   it should "list issues" in {
-    val sif = new ListUserIssuesFunction
-    val response = sif.invoke("31", TestCredentials.Token)
+    val tempRepo = newPopulatedTemporaryRepo()
+    val issue = createIssue(tempRepo, "test issue", "Issue body")
+    issue.addAssignees(gitHub.getUser("alankstewart"))
+
+    val f = new ListUserIssuesFunction
+    val response = f.invoke("1", Token)
     response.status shouldBe Status.Success
     val body = response.body
     body shouldBe defined
     body.get.str shouldBe defined
     val issues = JsonUtils.fromJson[Seq[GitHubIssue]](body.get.str.get)
-    issues.size shouldBe >(0)
+    issues.size should be > 0
   }
 
   it should "fail to list issues with bad token" in {
-    val sif = new ListUserIssuesFunction
-    val response = sif.invoke("1", "comfoobar")
+    val f = new ListUserIssuesFunction
+    val response = f.invoke("1", "comfoobar")
     response.status shouldBe Status.Failure
   }
 }
--- src/test/scala/com/atomist/rug/function/github/SearchIssuesFunctionTest.scala
+++ src/test/scala/com/atomist/rug/function/github/SearchIssuesFunctionTest.scala
@@ -1,26 +1,31 @@
 package com.atomist.rug.function.github
 
 import com.atomist.rug.function.github.GitHubSearchIssues.GitHubIssue
+import com.atomist.rug.function.github.TestCredentials.Token
 import com.atomist.rug.spi.Handlers.Status
 import com.atomist.util.JsonUtils
-import org.scalatest.{FlatSpec, Matchers}
 
-class SearchIssuesFunctionTest extends FlatSpec with Matchers {
+class SearchIssuesFunctionTest extends GitHubFunctionTest(Token) {
 
   it should "search issues" in {
-    val sif = new SearchIssuesFunction
-    val response = sif.invoke(null, "github-commands", "atomisthqa", TestCredentials.Token)
+    val tempRepo = newPopulatedTemporaryRepo()
+    val issue = createIssue(tempRepo, "test issue", "Issue body")
+    issue.addAssignees(gitHub.getUser("alankstewart"))
+
+    val f = new SearchIssuesFunction
+    val response = f.invoke(null, tempRepo.getName, tempRepo.getOwnerName, Token)
     response.status shouldBe Status.Success
     val body = response.body
     body shouldBe defined
     body.get.str shouldBe defined
     val issues = JsonUtils.fromJson[Seq[GitHubIssue]](body.get.str.get)
-    issues.size shouldBe >(0)
+    issues should have size 1
+    issues.head.title shouldBe "test issue"
   }
 
   it should "fail to search issues in non-existent repo" in {
-    val sif = new SearchIssuesFunction
-    val response = sif.invoke(null, "github-commands", "atomisthq", TestCredentials.Token)
+    val f = new SearchIssuesFunction
+    val response = f.invoke(null, "github-commands", "comfoobar", Token)
     response.status shouldBe Status.Failure
   }
 }
