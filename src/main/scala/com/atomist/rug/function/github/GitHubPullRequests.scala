0f5ce43920a7c62961497191144a4b7583a9d6e8
Alan Stewart <alan@atomist.com>
2017-03-29 15:19:04 +0100

    Mapped GHPullRequest

--- src/main/scala/com/atomist/rug/function/github/GitHubPullRequests.scala
+++ src/main/scala/com/atomist/rug/function/github/GitHubPullRequests.scala
@@ -3,23 +3,37 @@ package com.atomist.rug.function.github
 import java.time.OffsetDateTime
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.kohsuke.github.GHPullRequest
 
 object GitHubPullRequests {
 
-  case class PullRequestStatus(var id: Long,
-                               var url: String,
-                               @JsonProperty("html_url") var htmlUrl: String,
-                               var number: Int,
-                               var title: String,
-                               var body: String,
-                               @JsonProperty("created_at") var createdAt: OffsetDateTime,
-                               @JsonProperty("updated_at") var updatedAt: OffsetDateTime,
-                               @JsonProperty("merged_at") var mergedAt: OffsetDateTime,
-                               @JsonProperty("closed_at") var closedAt: OffsetDateTime,
-                               var head: PullRequestBranch,
-                               var base: PullRequestBranch,
-                               var state: String)
+  import GitHubFunction._
+
+  case class PullRequest(id: Long,
+                         url: String,
+                         @JsonProperty("html_url") var htmlUrl: String,
+                         number: Int,
+                         title: String,
+                         body: String,
+                         @JsonProperty("created_at") createdAt: OffsetDateTime,
+                         @JsonProperty("updated_at") updatedAt: OffsetDateTime,
+                         @JsonProperty("merged_at") mergedAt: OffsetDateTime,
+                         @JsonProperty("closed_at") closedAt: OffsetDateTime,
+                         head: PullRequestBranch,
+                         base: PullRequestBranch,
+                         state: String,
+                         merged: Boolean,
+                         mergeable: Boolean,
+                         @JsonProperty("mergeable_state") mergeableState: String,
+                         comments: Int,
+                         @JsonProperty("review_comments") reviewComments: Int)
 
   case class PullRequestBranch(ref: String, sha: String)
 
+  def mapPullRequest(pr: GHPullRequest): PullRequest =
+    PullRequest(pr.getId, pr.getUrl.toExternalForm, pr.getHtmlUrl.toExternalForm, pr.getNumber, pr.getTitle,
+      pr.getBody, convertDate(pr.getCreatedAt), convertDate(pr.getUpdatedAt), convertDate(pr.getMergedAt),
+      convertDate(pr.getClosedAt), PullRequestBranch(pr.getHead.getRef, pr.getHead.getSha),
+      PullRequestBranch(pr.getBase.getRef, pr.getBase.getSha), pr.getState.name(), pr.isMerged, pr.getMergeable,
+      pr.getMergeableState, pr.getCommentsCount, pr.getReviewComments)
 }
