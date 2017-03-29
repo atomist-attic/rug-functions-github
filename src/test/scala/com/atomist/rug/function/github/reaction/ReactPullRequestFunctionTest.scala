fd9b4201ce4731262af63c01339cbd8c5fc60d24
Clay McCoy <clay@atomist.com>
2017-03-29 15:23:48 +0100

    It is possible to add a reaction to a pull request if you treat it like an issue

--- src/test/scala/com/atomist/rug/function/github/reaction/ReactPullRequestFunctionTest.scala
+++ src/test/scala/com/atomist/rug/function/github/reaction/ReactPullRequestFunctionTest.scala
@@ -3,13 +3,14 @@ package com.atomist.rug.function.github.reaction
 import com.atomist.rug.function.github.GitHubFunctionTest
 import com.atomist.rug.function.github.TestCredentials._
 import com.atomist.rug.spi.Handlers.Status
+import com.atomist.util.JsonUtils
+import org.kohsuke.github.ReactionContent
 
 import scala.collection.JavaConverters._
 
 class ReactPullRequestFunctionTest extends GitHubFunctionTest(Token) {
 
-  it should "currently be unable to add reaction to pull request" in {
-    // when this capability works on GitHub, a failing test here will let us know
+  it should "add reaction to pull request" in {
     val tempRepo = newPopulatedTemporaryRepo()
     val sha1 = tempRepo.listCommits.asScala.toSeq.head.getSHA1
     tempRepo.createRef("refs/heads/test", sha1)
@@ -19,11 +20,14 @@ class ReactPullRequestFunctionTest extends GitHubFunctionTest(Token) {
 
     val f = new ReactPullRequestFunction
     val response = f.invoke("+1", pullRequest.getNumber, tempRepo.getName, tempRepo.getOwnerName, Token)
+    response.status shouldBe Status.Success
+    val result = JsonUtils.fromJson[Map[String, Any]](response.body.get.str.get)
+    result("content") shouldBe "+1"
 
-    // reading and writing reactions on Pull Requests is not supported by GitHub today
-    response.status shouldBe Status.Failure
-    intercept[Error] {
-      pullRequest.listReactions().asScala.toSeq
-    }
+    // must look pull request up as an Issue to see reactions
+    val prAsIssue = tempRepo.getIssue(pullRequest.getNumber)
+    val actualReactions = prAsIssue.listReactions().asScala.toSeq
+    actualReactions.size shouldBe 1
+    actualReactions.head.getContent shouldBe ReactionContent.PLUS_ONE
   }
 }
