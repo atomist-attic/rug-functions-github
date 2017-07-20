d8bde49ead5044e7eb497dce7a4035e00af881a1
Christian Dupuis <cd@atomist.com>
2017-07-19 21:17:35 -0400

    add RaisePullRequestFunction

--- src/main/scala/com/atomist/rug/function/github/pullrequest/RaisePullRequestFunction.scala
+++ src/main/scala/com/atomist/rug/function/github/pullrequest/RaisePullRequestFunction.scala
@@ -7,35 +7,37 @@ import com.atomist.rug.spi.{AnnotatedRugFunction, FunctionResponse, StringBodyOp
 import com.typesafe.scalalogging.LazyLogging
 
 /**
-  * Merge a pull request.
+  * Raise a pull request.
   */
-class MergePullRequestFunction
+class RaisePullRequestFunction
   extends AnnotatedRugFunction
     with LazyLogging
     with GitHubFunction {
 
-  @RugFunction(name = "merge-github-pull-request", description = "Merges a pull request",
+  @RugFunction(name = "raise-github-pull-request", description = "Raise a pull request",
     tags = Array(new Tag(name = "github"), new Tag(name = "issues"), new Tag(name = "pr")))
-  def invoke(@Parameter(name = "issue") number: Int,
+  def invoke(@Parameter(name = "title") title: String,
+             @Parameter(name = "body") body: String,
+             @Parameter(name = "base") base: String,
+             @Parameter(name = "head") head: String,
              @Parameter(name = "repo") repo: String,
              @Parameter(name = "owner") owner: String,
              @Parameter(name = "apiUrl") apiUrl: String,
              @Secret(name = "user_token", path = "github://user_token?scopes=repo") token: String): FunctionResponse = {
 
-    logger.info(s"Invoking mergePullRequest with number '$number', owner '$owner', repo '$repo' and token '${safeToken(token)}'")
+    logger.info(s"Invoking raisePullRequest with title '$title', owner '$owner', repo '$repo', base '$base', head '$head' and token '${safeToken(token)}'")
 
     try {
       val ghs = gitHubServices(token, apiUrl)
       ghs.getRepository(repo, owner)
         .map(repository => {
-          val pullRequest = repository.getPullRequest(number)
-          pullRequest.merge(null)
-          FunctionResponse(Status.Success, Some(s"Successfully merged pull request `$number`"), None)
+          val pullRequest = repository.createPullRequest(title, head, base, body)
+          FunctionResponse(Status.Success, Some(s"Successfully raised pull request `${pullRequest.getNumber}`"), None)
         })
         .getOrElse(FunctionResponse(Status.Failure, Some(s"Failed to find repository `$repo` for owner `$owner`"), None, None))
     } catch {
       case e: Exception =>
-        val msg = s"Failed to merge pull request `$number`"
+        val msg = s"Failed to raise pull request `$title`"
         logger.error(msg, e)
         FunctionResponse(Status.Failure, Some(msg), None, StringBodyOption(e.getMessage))
     }
